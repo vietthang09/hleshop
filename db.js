c4e3fb2668a6377be1fa1ae4fd69161554f7cab7
vthcvn <vthcvn@gmail.com>
2023-01-29 12:34:47 +0700

    feat: update database information

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '',
-    database: 'doantotnghiep',
+    user: 'sql12594110',
+    host: 'sql12.freesqldatabase.com',
+    password: 'rWnt8pn4Dv',
+    database: 'sql12594110',
 });
 
 module.exports = db;
